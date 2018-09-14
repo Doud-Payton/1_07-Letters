00b4efe830da4c54bbece2ee882a5f19d1acdee5
19payton.do@stduent.sfsd.us <19payton.do@stduent.sfsd.us>
2018-09-14 08:19:47 -0700

    first commit

---
+++ src/Letter.java
@@ -0,0 +1,25 @@
+
+public class Letter {
+
+	public static void para1() {
+		System.out.print("Dear Me,\n\t");
+		System.out.print("<insert good paragraph here>");
+		System.out.println("Sincerely, \n\tMe");
+	}
+	public static void para2() {
+		System.out.print("Dear Ma, \n\t");
+		System.out.print("<put another good paragraph here>");
+		System.out.println("Bye,\n\tPayton");
+	}
+	public static void para3() {
+		System.out.print("Dear Someone");
+		System.out.print("<Allan, please add details>");
+		System.out.println("Writing,\n\tPayton");
+	}
+	public static void main(String[] args) {
+		para1();
+		para2();
+		para3();
+	}
+
+}
